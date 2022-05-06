b32e188dc7e2a16195bd5319791e7a488164620b
s.chaikin <s.chaikin@ra-wm.ru>
2022-05-06 17:53:16 +0300

    feat: add home route

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="app_main")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
